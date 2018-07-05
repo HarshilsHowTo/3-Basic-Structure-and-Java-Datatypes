a4f5530716d66388569fdc9456bdd11368a55628
Harshil <harshils.how.to@gmail.com>
2018-07-05 19:48:14 +0100

    added my github name

---
+++ HelloWorld/src/J4Functions.java
@@ -0,0 +1,7 @@
+public class J4Functions {
+
+    public static void main(String[] args) {
+        System.out.println("w");
+    }
+
+}
---
+++ HelloWorld/src/JavaSeries3Datatypes.java
@@ -0,0 +1,29 @@
+public class JavaSeries3Datatypes {
+
+
+
+    public static void main(String[] args){
+        String myString = "14 + 3";
+        char myChar = 'j';
+
+        boolean myBool = false;
+
+        byte myByte = -128;
+        int myNumber = 14;
+        int myNumberTwo = 3;
+        int myNumberThree = myNumber + myNumberTwo;
+
+        long myLong = -4353;
+
+        short myShort = -78;
+
+//        int decimal = 3.14;
+        float decimal = 3.14f;
+        double decimals = 4.2334;
+
+        System.out.println(decimal);
+    }
+
+}
+
+
